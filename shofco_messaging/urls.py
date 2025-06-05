0fb9ee0bceb3a8258ddbf286be016d3dab6a48df
Rodgers1998 <companybells@gmail.com>
2025-06-05 10:15:53 +0300

    updated

--- shofco_messaging/urls.py
+++ shofco_messaging/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.shortcuts import redirect
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -10,3 +11,5 @@ urlpatterns = [
     path("survey/", include("surveys.urls", namespace="surveys")),
     path('', lambda request: redirect('messaging/', permanent=False)),  # 👈 this line fixes it
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
